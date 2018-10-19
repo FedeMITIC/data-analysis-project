2b6d4940fe81a1276f00b72398965dc0bad630ff
Federico Macchi <federico.macchi@mail.polimi.it>
2018-10-20 00:19:33 +0300

    Improved neural network algorithm (now loss = 0.00049346) -> still doesn't work.

--- Neural Network.py
+++ Neural Network.py
@@ -1,73 +1,53 @@
+# Result: accuracy ~30%
 # Import libraries
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
-
-
-def display_data(data, name):
-    print(f'{name}:\n')
-    print(data)
-    print('\n')
-
-
-def normalize_data(data):
-    scaler = StandardScaler()
-    scaler.fit(data)
-    return scaler.transform(data)
-
-
-def predict(data):
-    return mlp.predict(data)
-
-
-def train_accuracy_challenge(train_data, train_labels):
-    # Expose the network globally
-    global mlp
-    # Train the model using 3 layers: each layer has a number of neurons that equal the features
-    mlp = MLPClassifier(hidden_layer_sizes=264)
-    mlp.fit(train_data, train_labels)
+from sklearn.model_selection import train_test_split
 
 
 def main():
+    # Load the files for accuracy
     test_data_accuracy = pd.read_csv('./accuracy/test_data.csv')
     train_data_accuracy = pd.read_csv('./accuracy/train_data.csv')
     train_labels_accuracy = pd.read_csv('./accuracy/train_labels.csv')
-
-    # reduce_features(test_data_accuracy)
-
-    # print(test_data_accuracy[0].shape)
-    print(len(test_data_accuracy))
-    print(len(train_data_accuracy))
-    print(len(train_labels_accuracy))
-
     # test_data_logloss = pd.read_csv('./log-loss/test_data.csv')
     # train_data_logloss = pd.read_csv('./log-loss/train_data.csv')
     # train_labels_logloss = pd.read_csv('./log-loss/train_labels.csv')
 
-    # display_data(test_data_accuracy, 'Test data accuracy')
-    # display_data(test_data_logloss, 'Test data logloss')
-    # display_data(train_data_accuracy, 'Train data accuracy')
-    # display_data(train_labels_accuracy, 'Train label accuracy')
-    # display_data(train_data_logloss, 'Train data logloss')
-    # display_data(train_labels_logloss, 'Train label logloss')
+    # Read the values
+    train_labels_accuracy = train_labels_accuracy.values
+    train_data_accuracy = train_data_accuracy.values
+    test_data_accuracy = test_data_accuracy.values
 
-    # Scales the training data
-    train_data_accuracy = normalize_data(train_data_accuracy)
-    test_data_accuracy = normalize_data(test_data_accuracy)
+    # Split the dataset
+    x_train, x_test, y_train, y_test = train_test_split(train_data_accuracy,
+                                                        train_labels_accuracy,
+                                                        test_size=0.33,
+                                                        random_state=42)
 
-    # Train the model for the accuracy challenge
-    train_accuracy_challenge(train_data_accuracy, train_labels_accuracy)
-    # predictions = predict(train_data_accuracy)
-    predictions = predict(test_data_accuracy)
-    for i in range(len(predictions)):
-        print(f'{i}: {predictions[i]}')
+    # Scale the data before feeding them in the Neural Network
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+    x_train = scaler.transform(x_train)
+    test_data_accuracy = scaler.transform(test_data_accuracy)
+
+    # Instantiate and train the Neural Network: final loss => Iteration 83, loss = 0.00049346
+    mlp = MLPClassifier(hidden_layer_sizes=(264, 264, 264), verbose=True, tol=0.00001)
+    mlp.fit(x_train, np.ravel(y_train))
+
+    # Outputs the predictions: array of 6544 (0 to 6543)
+    predictions = mlp.predict(test_data_accuracy)
+
+    class_names = ['Pop_Rock', 'Electronic', 'Rap', 'Jazz', 'Latin', 'RnB',
+                   'International', 'Country', 'Reggae', 'Blues']
     for j in range(1, 11, 1):
-        index = np.where(predictions == j)
-        print(f'{j}: {index[0].size}')
+        index_acc = np.where(predictions == j)
+        print(f'{class_names[j - 1]}: {index_acc[0].size}')
+    print(f'Last value: {predictions[len(predictions) - 1]}')
     id_num = range(1, len(predictions) + 1)
     df = pd.DataFrame({'Sample_id': id_num, 'Sample_label': predictions})
-    df.columns = ['Sample_id', 'Sample_label']
     df.to_csv('attempt.csv')
 
 
