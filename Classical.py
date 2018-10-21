cb49b342b8d48cbc83588968a3abe6785f6a2081
Federico Macchi <federico.macchi@mail.polimi.it>
2018-10-21 18:36:03 +0300

    Implemented RandomForestClassifier algorithm

--- Classical.py
+++ Classical.py
@@ -1,62 +1,71 @@
+# Result: accuracy ~30%
 # Import libraries
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
+import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 
-def normalize_data(data):
-    scaler = StandardScaler()
-    scaler.fit(data)
-    return scaler.transform(data)
-
-
-def predict(data):
-    return mlp.predict(data)
-
-
-def train_accuracy_challenge(train_data, train_labels):
-    # Expose the network globally
-    global mlp
-    # Train the model using 3 layers: each layer has a number of neurons that equal the features
-    mlp = MLPClassifier(hidden_layer_sizes=264)
-    mlp.fit(train_data, train_labels)
+class_names = ['Pop_Rock', 'Electronic', 'Rap', 'Jazz', 'Latin', 'RnB',
+               'International', 'Country', 'Reggae', 'Blues']
 
 
 def main():
-    # test_data_logloss = pd.read_csv('./log-loss/test_data.csv')
-    # train_data_logloss = pd.read_csv('./log-loss/train_data.csv')
-    # train_labels_logloss = pd.read_csv('./log-loss/train_labels.csv')
+    # Load the files for accuracy
     test_data_accuracy = pd.read_csv('./accuracy/test_data.csv')
     train_data_accuracy = pd.read_csv('./accuracy/train_data.csv')
     train_labels_accuracy = pd.read_csv('./accuracy/train_labels.csv')
+    # test_data_logloss = pd.read_csv('./log-loss/test_data.csv')
+    # train_data_logloss = pd.read_csv('./log-loss/train_data.csv')
+    # train_labels_logloss = pd.read_csv('./log-loss/train_labels.csv')
 
+    print('Analyzing training labels:')
+    for i in range(1, 11, 1):
+        print(f'Label {i}({class_names[i - 1]}): {len(np.where(train_labels_accuracy.values == i)[0])} ({round((len(np.where(train_labels_accuracy.values == i)[0]) / len(train_labels_accuracy)) * 100, 2)}%)')
+
+    # Read the values
+    train_labels_accuracy = train_labels_accuracy.values
+    train_data_accuracy = train_data_accuracy.values
     test_data_accuracy = test_data_accuracy.values
-    test_data_resized = np.empty((len(test_data_accuracy), 4))
-    for i in range(test_data_accuracy.shape[0]):
-        # ID
-        test_data_resized[i, 0] = test_data_accuracy[i, 0]
-        # Rhythm mean
-        test_data_resized[i, 1] = np.mean(test_data_accuracy[i, 1:169])
-        # Chroma mean
-        test_data_resized[i, 2] = np.mean(test_data_accuracy[i, 170:217])
-        # MFCCs mean
-        test_data_resized[i, 3] = np.mean(test_data_accuracy[i, 218:265])
-    print(test_data_resized)
-    # Reduce the dimensions of the test_data to avoid overfitting
-    # test_data_accuracy = train_test_split(test_data_accuracy)
 
-    # for i in range(len(predictions)):
-    #     print(f'{i}: {predictions[i]}')
-    # for j in range(1, 11, 1):
-    #     index = np.where(predictions == j)
-    #     print(f'{j}: {index[0].size}')
-    # id_num = range(1, len(predictions) + 1)
-    # df = pd.DataFrame({'Sample_id': id_num, 'Sample_label': predictions})
-    # df.columns = ['Sample_id', 'Sample_label']
-    # df.to_csv('attempt.csv')
+    test_data_resized = np.empty((len(test_data_accuracy), 3))
+    train_data_resized = np.empty((len(train_data_accuracy), 3))
+    for i in range(test_data_accuracy.shape[0]):
+        # Rhythm mean
+        test_data_resized[i, 0] = np.mean(test_data_accuracy[i, 1:169])
+        # Chroma mean
+        test_data_resized[i, 1] = np.mean(test_data_accuracy[i, 170:217])
+        # MFCCs mean
+        test_data_resized[i, 2] = np.mean(test_data_accuracy[i, 218:265])
+
+    for i in range(train_data_accuracy.shape[0]):
+        # Rhythm mean
+        train_data_resized[i, 0] = np.mean(train_data_accuracy[i, 1:169])
+        # Chroma mean
+        train_data_resized[i, 1] = np.mean(train_data_accuracy[i, 170:217])
+        # MFCCs mean
+        train_data_resized[i, 2] = np.mean(train_data_accuracy[i, 218:265])
+
+    # Scale the data before feeding them in the Neural Network
+    scaler = StandardScaler()
+    train_data_resized = scaler.fit_transform(train_data_resized)
+    test_data_resized = scaler.transform(test_data_resized)
+
+    classifier = RandomForestClassifier(n_estimators=1000, criterion='gini', random_state=42, verbose=1)
+    classifier.fit(train_data_resized, np.ravel(train_labels_accuracy))
+
+    # Outputs the predictions: array of 6544 (0 to 6543)
+    predictions = classifier.predict(test_data_resized)
+    print(f'Total predictions: {len(predictions)}')
+
+    for j in range(1, 11, 1):
+        index_acc = np.where(predictions == j)
+        print(f'{class_names[j - 1]}: {index_acc[0].size} ({round((index_acc[0].size / len(predictions)) * 100, 2)}%)')
+    print(f'Last value: {predictions[len(predictions) - 1]}')
+    id_num = range(1, len(predictions) + 1)
+    df = pd.DataFrame({'Sample_id': id_num, 'Sample_label': predictions}, columns=['Sample_id', 'Sample_label'])
+    df.to_csv('treeforest.csv')
 
 
 if __name__ == "__main__":
