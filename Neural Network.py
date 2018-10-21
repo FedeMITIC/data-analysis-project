800fb091b053586eabd06fd1dedf1735a2b5b9f9
Federico Macchi <federico.macchi@mail.polimi.it>
2018-10-21 18:25:28 +0300

    Updated script, still not working

--- Neural Network.py
+++ Neural Network.py
@@ -16,6 +16,9 @@ def main():
     # train_data_logloss = pd.read_csv('./log-loss/train_data.csv')
     # train_labels_logloss = pd.read_csv('./log-loss/train_labels.csv')
 
+    for i in range(1, 11, 1):
+        print(f'Label {i}: {len(np.where(train_labels_accuracy.values == i)[0])}')
+
     # Read the values
     train_labels_accuracy = train_labels_accuracy.values
     train_data_accuracy = train_data_accuracy.values
@@ -24,30 +27,39 @@ def main():
     # Split the dataset
     x_train, x_test, y_train, y_test = train_test_split(train_data_accuracy,
                                                         train_labels_accuracy,
-                                                        test_size=0.33,
+                                                        test_size=0.1,
                                                         random_state=42)
 
     # Scale the data before feeding them in the Neural Network
     scaler = StandardScaler()
-    scaler.fit(x_train)
-    x_train = scaler.transform(x_train)
+    x_train = scaler.fit_transform(x_train)
     test_data_accuracy = scaler.transform(test_data_accuracy)
 
-    # Instantiate and train the Neural Network: final loss => Iteration 83, loss = 0.00049346
-    mlp = MLPClassifier(hidden_layer_sizes=(264, 264, 264), verbose=True, tol=0.00001)
+    # Instantiate and train the Neural Network: final loss => Iteration 166, loss = 0.00032670
+    mlp = MLPClassifier(activation='relu',
+                        alpha=0.0001,
+                        hidden_layer_sizes=(263, 263),
+                        learning_rate='constant',
+                        max_iter=1000,
+                        shuffle=True,
+                        solver='adam',
+                        tol=0.000001,
+                        verbose=True
+                        )
     mlp.fit(x_train, np.ravel(y_train))
 
     # Outputs the predictions: array of 6544 (0 to 6543)
     predictions = mlp.predict(test_data_accuracy)
+    print(f'Total predictions: {len(predictions)}')
 
     class_names = ['Pop_Rock', 'Electronic', 'Rap', 'Jazz', 'Latin', 'RnB',
                    'International', 'Country', 'Reggae', 'Blues']
     for j in range(1, 11, 1):
         index_acc = np.where(predictions == j)
-        print(f'{class_names[j - 1]}: {index_acc[0].size}')
+        print(f'{class_names[j - 1]}: {index_acc[0].size} ({round((index_acc[0].size / len(predictions)) * 100, 2)}%)')
     print(f'Last value: {predictions[len(predictions) - 1]}')
     id_num = range(1, len(predictions) + 1)
-    df = pd.DataFrame({'Sample_id': id_num, 'Sample_label': predictions})
+    df = pd.DataFrame({'Sample_id': id_num, 'Sample_label': predictions}, columns=['Sample_id', 'Sample_label'])
     df.to_csv('attempt.csv')
 
 
